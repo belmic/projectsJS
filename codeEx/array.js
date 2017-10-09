608a22c4f6641bbddd8ff9379d201e882e5a2832
belmic <mic.belov@gmail.com>
2017-10-09 23:20:22 +0300

    Update array.js

--- codeEx/array.js
+++ codeEx/array.js
@@ -1,6 +1,16 @@
 //Examples of array functions and commands
-// new line
-console.log("Arrays");
-//
-//new new
-//
+//1. Converting array from string to numbers
+
+stringArray.map(Number);
+
+//2. Sorting array of numbers from 1-100
+numArray.sort(function(a, b)
+{
+    return a - b;
+});
+//From 100 - 1
+numArray.sort(function(a, b)
+{
+    return b - a;
+});
+

bd92569f725b08ebcea30b6d4c5b784c4438d406
Mikhail Belov <mic_belov@MacBook-Pro-Mikhail.local>
2017-10-12 21:30:40 +0300

    diagonal dif

---
+++ hackerRank/Diagonal Difference.js
@@ -0,0 +1,37 @@
+process.stdin.resume();
+process.stdin.setEncoding('ascii');
+
+var input_stdin = "";
+var input_stdin_array = "";
+var input_currentline = 0;
+
+process.stdin.on('data', function (data) {
+    input_stdin += data;
+});
+
+process.stdin.on('end', function () {
+    input_stdin_array = input_stdin.split("\n");
+    main();
+});
+
+function readLine() {
+    return input_stdin_array[input_currentline++];
+}
+
+/////////////// ignore above this line ////////////////////
+
+function main() {
+    var n = parseInt(readLine());
+    var a = [];
+    var lDiag = 0;
+    var rDiag = 0;
+    for(a_i = 0; a_i < n; a_i++){
+       a[a_i] = readLine().split(' ');
+       a[a_i] = a[a_i].map(Number);
+    }
+    for (let i = 0; i < n; i++){
+        lDiag += a[i][i];
+        rDiag += a[i][n-1-i];
+    }
+    console.log(Math.abs(lDiag-rDiag));
+}
